061ea2371e4ec6171d5495581ae19f6561075121
Manikm07-nikk <manikmukhija6666@gmail.com>
2024-04-17 02:23:44 +0530

    added time to stop caching

--- src/app/pages/api/get/route.js
+++ src/app/pages/api/get/route.js
@@ -2,8 +2,12 @@ import { NextResponse } from "next/server";
 
 export async function GET(){
   try {
-    const response = await fetch('http://52.66.71.14:8080/api/dns');
+    const timestamp = Date.now();
+const response = await fetch(`http://52.66.71.14:8080/api/dns?timestamp=${timestamp}`);
+
+
     const data = await response.json();
+    
     return NextResponse.json(data);
   } catch (error) {
     console.error('Error:', error);
